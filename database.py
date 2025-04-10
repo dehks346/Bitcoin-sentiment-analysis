942720ba2dcd52192fd5786ded3d716a71a13651
henry <@>
2025-04-10 10:33:40 +0100

    working

--- database.py
+++ database.py
@@ -6,7 +6,9 @@ from models import Base
 DATABASE_URL = "sqlite:///./news.db"  # Use triple slashes for SQLite local file DB
 
 # Create the engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 # SessionLocal is used to get a database session object
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
