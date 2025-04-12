f65e4231068c2d20057ff665e26aa36efb4c7eca
henry <@>
2025-04-12 12:05:06 +0100

    .

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-DATABASE_URL = "sqlite:////Users/henry/Documents/datasi/news.db"
+DATABASE_URL = "sqlite:///news.db" 
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
